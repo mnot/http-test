6ec30291aa40dd4ad5e447132433fee7bc16ef1e
Mark Nottingham <mnot@mnot.net>
2010-10-10 22:35:10 -0700

    sort out results; dynamic headers

--- ua-browser/tests/cache.js
+++ ua-browser/tests/cache.js
@@ -1,8 +1,8 @@
-function pastExpires(n) {
+function minusTen(n) {
   return new Date(n.valueOf() - (10 * 1000)).toUTCString();
 };
 
-function futureExpires(n) {
+function plusTen(n) {
   return new Date(n.valueOf() + (10 * 1000)).toUTCString();
 };
 
@@ -10,24 +10,35 @@ this.tests = [
   {
     'id': 'expires_past',
     'desc': 'Expires in the past', 
-    'res_hdrs': {'Expires': pastExpires},
+    'status': ['200', 'OK'],
+    'res_hdrs': {'Expires': minusTen},
     'fresh_for': 0
   },
   {
     'id': 'expires_future',
     'desc': 'Expires in the future', 
-    'res_hdrs': {'Expires': futureExpires},
+    'status': ['200', 'OK'],
+    'res_hdrs': {'Expires': plusTen},
     'fresh_for': 10
+  },
+  {
+    'id': 'expires_invalid',
+    'desc': 'Invalid Expires header',
+    'status': ['200', 'OK'],
+    'res_hdrs': {'Expires': 'foo'},
+    'fresh_for': 0
   },
   {
     'id': 'cc_ma',
     'desc': 'Cache-Control: max-age=10',
+    'status': ['200', 'OK'],
     'res_hdrs': {'Cache-Control': 'max-age=10'},
     'fresh_for': 10
   },
   {
     'id': 'age',
-    'desc': 'Cache-Control: max-age=10',
+    'desc': 'Cache-Control: max-age=10 + Age: 5',
+    'status': ['200', 'OK'],
     'res_hdrs': {
       'Cache-Control': 'max-age=10',
       'Age': '5'
@@ -35,11 +46,65 @@ this.tests = [
     'fresh_for': 5
   },
   {
+    'id': 'old_date',
+    'desc': 'Cache-Control: max-age=10 + Old Date', 
+    'status': ['200', 'OK'],
+    'res_hdrs': {
+      'Cache-Control': 'max-age=5',
+      'Date': minusTen
+    },
+    'fresh_for': 0
+  },
+  {
     'id': 'cc_nc',
     'desc': 'Cache-Control: no-cache',
+    'status': ['200', 'OK'],
     'res_hdrs': {'Cache-Control': 'no-cache'},
     'fresh_for': 0
   },
+  {
+    'id': 'cc_ns',
+    'desc': 'Cache-Control: no-store',
+    'status': ['200', 'OK'],
+    'res_hdrs': {'Cache-Control': 'no-store'},
+    'fresh_for': 0
+  },
+  {
+    'id': 'cc_precedence',
+    'desc': 'Cache-Control: max-age precedence over Expires', 
+    'status': ['200', 'OK'],
+    'res_hdrs': {
+      'Cache-Control: max-age=0',
+      'Expires': plusTen
+    },
+    'fresh_for': 0
+  },
+  {
+    'id': 'vary',
+    'desc': 'Vary caching', 
+    'status': ['200', 'OK'],
+    'res_hdrs': {
+      'Cache-Control: max-age=100',
+      'Vary: *'
+    },
+    'fresh_for': 0
+  },
+  {
+    'id': 'unk_status',
+    'desc': 'Unknown Status Code', 
+    'status': ['250', 'Whatever'],
+    'res_hdrs': {'Cache-Control': 'max-age=50'},
+    'fresh_for': 0
+  },
+  /*
+  {
+    'id': '',
+    'desc': '', 
+    'status': ['200', 'OK'],
+    'res_hdrs': {},
+    'fresh_for': 0
+  },
+  */
 ];
 
 var asset_types = ['html', 'img', 'script', 'css', 'iframe'];
